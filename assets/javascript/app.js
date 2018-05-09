85c9c3f9bf1b4274fa03a74770b0c347cba99b71
Jonas Grumby <paololavita@Paolos-MacBook-Pro.local>
2018-05-09 18:18:12 -0400

    6th

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,57 @@
+// use the 1-customer-object.html for the questions and answer section
 $(document).ready(function(){
     $("#submit-button").hide();
     $("#start-button").click(function() {
         $("#start-button").hide();
         $("#submit-button").show();
-        
+        // $(this).parent().remove();
+
+        // *** Timer that runs down to 0 ***
         var number = 60;
         var intervalId;
 
         var questions = {
             questionsArray: [{
-                question: "Who at William Osler is the most useless and biggest waste to the Ontario Taxpayer in general?",
-                a: "Elio D",
-                b: "John Go",
-                c: "Janet M",
-                d: "James M",
-                e: "Joe C",
-                f: "All the Above"
-
+                question: "What is the fastest bird on foot?",
+                a: "Ostrich",
+                b: "Sparrow",
+                c: "Eagle",
+                d: "Woodpecker",
             // },
             // {
-            //  question: "Question 2?",
-            //  a: "1",
-            //  b: "2",
-            //  c: "3",
-            //  d: "4",
+            //  question: "2) How long is one regular term for a U.S. Representative?",
+            //  a: "3 years",
+            //  b: "2 years",
+            //  c: "4 years",
+            //  d: "5 years",
             // },
             // {
-            //  question: "Question 3?",
-            //  a: "A",
-            //  b: "B",
-            //  c: "C",
-            //  d: "D",
+            //  question: "3) Which of the following states is not on the Gulf of Mexico?",
+            //  a: "Georgia",
+            //  b: "Alabama",
+            //  c: "Texas",
+            //  d: "Mississippi",
             // },
             // {
-            //  question: "Question 4?",
-            //  a: "W",
-            //  b: "X",
-            //  c: "Y",
-            //  d: "Z",
+            //  question: "4) What white, puffy clouds are known as 'fair weather' clouds?",
+            //  a: "Cumulus",
+            //  b: "Cumulonimbus",
+            //  c: "Stratus",
+            //  d: "Cirrus",
             // },
             // {
-            //  question: "Question 5?",
-            //  a: "1",
-            //  b: "2",
-            //  c: "3",
-            //  d: "4",
+            //  question: "5) Between 1455 and 1485, the War of the Roses took place in what country?",
+            //  a: "France",
+            //  b: "England",
+            //  c: "Germany",
+            //  d: "Spain",
             // },
             // {
-            //  question: "Question 6?",
-            //  a: "A",
-            //  b: "B",
-            //  c: "C",
-            //  d: "D",
+            //  question: "6) It's three fifteen PM. What is the degree difference between the hour hand and minute hand?",
+            //  a: "0 degrees",
+            //  b: "6 degrees",
+            //  c: "7.5 degrees",
+            //  d: "90 degrees",
             }]
         };
 
@@ -62,7 +62,7 @@ $(document).ready(function(){
                 $("#timer").html("<div>Time Left on the Clock: " + number + "</div>");
                 if (number === 0) {
                     stop();
-                    alert("Time Up!");
+                    alert("Unfortunately your time is up!");
                 }
             }
             function stop() {
@@ -75,30 +75,24 @@ $(document).ready(function(){
             + "<div><input type = 'radio' name = " + i + " value = " + questions.questionsArray[i].a + " id = 'disable1' " + i + ">" + questions.questionsArray[i].a + "</div>"
             + "<div><input type = 'radio' name = " + i + " value = " + questions.questionsArray[i].b + " id = 'disable2' " + i + ">" + questions.questionsArray[i].b + "</div>"
             + "<div><input type = 'radio' name = " + i + " value = " + questions.questionsArray[i].c + " id = 'disable3' " + i + ">" + questions.questionsArray[i].c + "</div>"
-            + "<div><input type = 'radio' name = " + i + " value = " + questions.questionsArray[i].d + " id = 'disable4' " + i + ">" + questions.questionsArray[i].d + "</div>"
-            + "<div><input type = 'radio' name = " + i + " value = " + questions.questionsArray[i].e + " id = 'disable5' " + i + ">" + questions.questionsArray[i].e + "</div>"
-            + "<div><input type = 'radio' name = " + i + " value = " + questions.questionsArray[i].f + " id = 'disable6' " + i + ">" + questions.questionsArray[i].f+ "</div><br>");
+            + "<div><input type = 'radio' name = " + i + " value = " + questions.questionsArray[i].d + " id = 'disable4' " + i + ">" + questions.questionsArray[i].d + "</div><br>");
         }
 
         $("#submit-button").click(function(){
             function answerKey(){
                 var answers = {
                     answersArray:
-                    [{answer0: "1"},
-                    {answer1: "C"},
-                    {answer2: "X"},
-                    {answer3: "4"},
-                    {answer4: "Y"},
-                    {answer5: "3"}]
+                    [{answer0: "Ostrich"},
+                    {answer1: "2 years"},
+                    {answer2: "Georgia"},
+                    {answer3: "Cumulus"},
+                    {answer4: "England"},
+                    {answer5: "7.5 degrees"}]
                 };
-
                 var clickA = $("#disable1").click(function(){console.log($(this).attr("checked", true).val());});
                 var clickB = $("#disable2").click(function(){console.log($(this).attr("checked", true).val());});
                 var clickC = $("#disable3").click(function(){console.log($(this).attr("checked", true).val());});
                 var clickD = $("#disable4").click(function(){console.log($(this).attr("checked", true).val());});
-                
-                console.log(answers[]);
-
                 if( clickA === answers.answersArray.answer0){
                     console.log("correct");
                 }
@@ -109,6 +103,7 @@ $(document).ready(function(){
             answerKey();
         });
 
+        // Run the functions after start button click
         run();
     })
 
