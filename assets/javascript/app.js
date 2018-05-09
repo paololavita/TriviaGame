7d1faa0000c47cd6c445942669b76f068b629f2e
Jonas Grumby <paololavita@Paolos-MacBook-Pro.local>
2018-05-09 16:21:49 -0400

    5th

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,15 @@
-// use the 1-customer-object.html for the questions and answer section
 $(document).ready(function(){
     $("#submit-button").hide();
     $("#start-button").click(function() {
         $("#start-button").hide();
         $("#submit-button").show();
-        // $(this).parent().remove();
-
-        // *** Timer that runs down to 0 ***
+        
         var number = 60;
         var intervalId;
 
         var questions = {
             questionsArray: [{
-                question: "Who at William Osler is the most useless and biggest waste to the Ontario Taxpayer?",
+                question: "Who at William Osler is the most useless and biggest waste to the Ontario Taxpayer in general?",
                 a: "Elio D",
                 b: "John Go",
                 c: "Janet M",
@@ -22,39 +19,39 @@ $(document).ready(function(){
 
             // },
             // {
-            //  question: "2) How long is one regular term for a U.S. Representative?",
-            //  a: "3 years",
-            //  b: "2 years",
-            //  c: "4 years",
-            //  d: "5 years",
+            //  question: "Question 2?",
+            //  a: "1",
+            //  b: "2",
+            //  c: "3",
+            //  d: "4",
             // },
             // {
-            //  question: "3) Which of the following states is not on the Gulf of Mexico?",
-            //  a: "Georgia",
-            //  b: "Alabama",
-            //  c: "Texas",
-            //  d: "Mississippi",
+            //  question: "Question 3?",
+            //  a: "A",
+            //  b: "B",
+            //  c: "C",
+            //  d: "D",
             // },
             // {
-            //  question: "4) What white, puffy clouds are known as 'fair weather' clouds?",
-            //  a: "Cumulus",
-            //  b: "Cumulonimbus",
-            //  c: "Stratus",
-            //  d: "Cirrus",
+            //  question: "Question 4?",
+            //  a: "W",
+            //  b: "X",
+            //  c: "Y",
+            //  d: "Z",
             // },
             // {
-            //  question: "5) Between 1455 and 1485, the War of the Roses took place in what country?",
-            //  a: "France",
-            //  b: "England",
-            //  c: "Germany",
-            //  d: "Spain",
+            //  question: "Question 5?",
+            //  a: "1",
+            //  b: "2",
+            //  c: "3",
+            //  d: "4",
             // },
             // {
-            //  question: "6) It's three fifteen PM. What is the degree difference between the hour hand and minute hand?",
-            //  a: "0 degrees",
-            //  b: "6 degrees",
-            //  c: "7.5 degrees",
-            //  d: "90 degrees",
+            //  question: "Question 6?",
+            //  a: "A",
+            //  b: "B",
+            //  c: "C",
+            //  d: "D",
             }]
         };
 
@@ -87,12 +84,12 @@ $(document).ready(function(){
             function answerKey(){
                 var answers = {
                     answersArray:
-                    [{answer0: "Ostrich"},
-                    {answer1: "2 years"},
-                    {answer2: "Georgia"},
-                    {answer3: "Cumulus"},
-                    {answer4: "England"},
-                    {answer5: "7.5 degrees"}]
+                    [{answer0: "1"},
+                    {answer1: "C"},
+                    {answer2: "X"},
+                    {answer3: "4"},
+                    {answer4: "Y"},
+                    {answer5: "3"}]
                 };
 
                 var clickA = $("#disable1").click(function(){console.log($(this).attr("checked", true).val());});
@@ -101,7 +98,7 @@ $(document).ready(function(){
                 var clickD = $("#disable4").click(function(){console.log($(this).attr("checked", true).val());});
                 
                 console.log(answers[]);
-                
+
                 if( clickA === answers.answersArray.answer0){
                     console.log("correct");
                 }
@@ -112,7 +109,6 @@ $(document).ready(function(){
             answerKey();
         });
 
-        // Run the functions after start button click
         run();
     })
 
